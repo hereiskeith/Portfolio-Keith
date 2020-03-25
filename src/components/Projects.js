fe890f173b8738d8b2b2de1ea3975c390539dadf
unknown <hereiskeith@gmail.com>
2020-03-25 09:19:01 -0400

    Deploy to heroku

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,25 @@
 import React from 'react';
 import BizMSM from '../static/bizmsm.png';
+import Dashboard from '../static/dashboard.png';
+import { bizmsmPics } from './bizmsmPics';
+import { dashboardPics } from "./dashboardPics";
+
+const renderBizmsmPics = () => {
+  return bizmsmPics.map(each => {
+    return (
+      <img className='bizmsmPic' src={each} />
+    )
+  })
+};
+
+const renderDashboardPics = () => {
+  return dashboardPics.map(each => {
+    return (
+      <img className='dashboardPic' src={each} />
+    )
+  })
+};
+
 
 const Projects = props => {
   return (
@@ -7,6 +27,7 @@ const Projects = props => {
       <h3 className='projectsTitle'>
         Projects
       </h3>
+
       <div className='eachProject'>
 
         <div className='logoWrapper'>
@@ -15,13 +36,44 @@ const Projects = props => {
 
         <div className='projectDetailWrapper'>
           <h4 className='projectTitle'>BizMSM</h4>
+
           <p className='projectDetail'>
             An awesome e-commerce app grouping local merchants and customers
           </p>
-          <div className='projectPhotoGallery'>
 
+          <div className='galleryWrapper'>
+            <div className='bizmsmPhotoGallery'>
+              {renderBizmsmPics()}
+            </div>
           </div>
+
         </div>
+
+      </div>
+
+      <div className='eachProject'>
+
+        <div className='logoWrapper'>
+          <img className='projectLogo' src={Dashboard} />
+        </div>
+
+        <div className='projectDetailWrapper'>
+          <h4 className='projectTitle'>React Dashboard</h4>
+
+          <p className='projectDetail'>
+            This awesome react dashboard is showing Bitcom trend with Highcharts charts
+            <br />
+            (https://github.com/hereiskeith/React-dashboard)
+          </p>
+
+          <div className='galleryWrapper'>
+            <div className='dashboardPhotoGallery'>
+              {renderDashboardPics()}
+            </div>
+          </div>
+
+        </div>
+
       </div>
     </div>
 
---
+++ src/components/bizmsmPics.js
@@ -0,0 +1,11 @@
+import Biz2 from '../static/bizmsm/2.jpg';
+import Biz3 from '../static/bizmsm/3.jpg';
+import Biz4 from '../static/bizmsm/4.jpg';
+import Biz5 from '../static/bizmsm/5.jpg';
+import Biz6 from '../static/bizmsm/6.jpg';
+import Biz7 from '../static/bizmsm/7.jpg';
+import Biz8 from '../static/bizmsm/8.jpg';
+import Biz9 from '../static/bizmsm/9.jpg';
+import Biz10 from '../static/bizmsm/10.jpg';
+
+export const bizmsmPics = [Biz2, Biz3, Biz4, Biz5, Biz6, Biz7, Biz8, Biz9, Biz10];
